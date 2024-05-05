b81c491ffd7f2736691d304fd1c887e9c197147f
Nafiznayan <abdullah191864@gmail.com>
2024-05-05 17:40:49 +0600

    Cloudconfig

--- cloudconfig.js
+++ cloudconfig.js
@@ -11,7 +11,7 @@ const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
       folder: 'wanderlust_DEV',
-      allowedFormat: ["png","jpg","jpeg"], 
+      allowerdFormat: ["png","jpg","jpeg"], 
       
     },
   });
