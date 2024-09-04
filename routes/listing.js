003dd8b1b9cbda52d58a1840e5548c06045b54ce
Nafiznayan <abdullah191864@gmail.com>
2024-09-04 12:41:26 +0600

    Remove mapbox

--- routes/listing.js
+++ routes/listing.js
@@ -2,33 +2,48 @@ const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../util/wrapAsync.js");
 const Listing = require("../models/listing.js");
-const {isLoggedIn,isOwner,validateListing} = require("../middlewire.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middlewire.js");
 const listingContoller = require("../controllers/listings.js");
-const multer  = require('multer')
-const {storage} = require("../cloudconfig.js");
+const multer = require("multer");
+const { storage } = require("../cloudconfig.js");
 const upload = multer({ storage });
 
 router
-    .route("/")
-    .get(wrapAsync(listingContoller.index))
-    .post(isLoggedIn,upload.single('listing[image]'),validateListing,wrapAsync(listingContoller.createListing));
-   
+  .route("/")
+  .get(wrapAsync(listingContoller.index))
+  .post(
+    isLoggedIn,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingContoller.createListing)
+  );
+
 // New rout
-router.get("/new",isLoggedIn,listingContoller.renderNewForm); 
+router.get("/new", isLoggedIn, listingContoller.renderNewForm);
 
-
-router.route("/:id")
-    .get(wrapAsync(listingContoller.showListing))
-    .put(isLoggedIn,isOwner,upload.single('listing[image]'),validateListing,wrapAsync(listingContoller.updateListing))
-    .delete(isLoggedIn,isOwner,wrapAsync(listingContoller.destroyListing));
+router
+  .route("/:id")
+  .get(wrapAsync(listingContoller.showListing))
+  .put(
+    isLoggedIn,
+    isOwner,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingContoller.updateListing)
+  )
+  .delete(isLoggedIn, isOwner, wrapAsync(listingContoller.destroyListing));
 
 // Edit rout
-router.get("/:id/edit",isLoggedIn,isOwner,wrapAsync(listingContoller.renderEditForm));    
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isOwner,
+  wrapAsync(listingContoller.renderEditForm)
+);
 
 module.exports = router;
 
-
-// Index rout 
+// Index rout
 // router.get("/",wrapAsync(listingContoller.index));
 // Show rout
 // router.get("/:id",wrapAsync(listingContoller.showListing));
