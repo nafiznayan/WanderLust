ecc53f7ff4515b747b37d03dd4b408b60a8fa637
Nafiznayan <abdullah191864@gmail.com>
2025-10-19 11:59:40 +0600

    Add images into readme

--- models/user.js
+++ models/user.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require("passport-local-mongoose");
+const passportLocalMongoose = require("passport-local-mongoose"); // integrates authentication features (username, password hashing, login verification, etc.) automatically through passport-local-mongoose.
 
 const userSchema = new Schema({
-   email :{
+  email: {
     type: String,
     required: true,
-   }
+  },
 });
 
 userSchema.plugin(passportLocalMongoose);
-module.exports = mongoose.model("User",userSchema);
+module.exports = mongoose.model("User", userSchema);
